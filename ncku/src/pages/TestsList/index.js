e43c601d42689387b0c06f71d1f6b4740bafb668
Karen-W-2002 <karenweidev@gmail.com>
2022-07-17 20:05:21 +0800

    Add Create Test btn

--- ncku/src/pages/TestsList/index.js
+++ ncku/src/pages/TestsList/index.js
@@ -10,35 +10,84 @@ const TestsList = () => {
 
   const [testData, setTestData] = useState(data);
 
-  function handleClick(e) {
+  function handleTestClick(e) {
+    console.log('test was clicked')
+  }
+
+  function handleRunTestClick(e) {
     e.preventDefault();
     /* Code to tell server to run test */
-    console.log('The link was clicked.');
+    console.log('run test was clicked.');
+  }
+
+  function handleReportClick(e) {
+    e.preventDefault();
+    console.log('report was clicked');
+  }
+
+  function returnDuration(seconds) {
+    if(seconds < 60) {
+      return seconds+"s"
+    }
+    else {
+      // Round up or down
+      return Math.round(seconds/60)+"m"
+    }
+  }
+
+  function createNewTest(e) {
+    /* Create New Test --> redirect to a new page */
+    console.log('creating new test');
   }
 
 {/* TODO: when the list is empty -> do something*/} 
 
   return(
-    <div className='app-container'>
-      <div className='list-title'>Tests</div>
+    <div className='testslist-container'>
+      <div className='testslist-top'>
+        {/* TODO: change (5) to something else using a function to calculate */}
+        <div className='list-title'><h1>Tests (5)</h1></div>
+        <div className='new-test-btn-container'>
+          <button className='new-test-btn' onClick={createNewTest}>Create Test</button>
+        </div>
+      </div>
+
       <table>
         <thead>
           <tr>
             <th>Name</th>
             <th>Update Date</th>
             <th>Users</th>
+            <th>Duration</th>
             <th>Actions</th>
+            <th>Reports</th>
           </tr>
         </thead>
         <tbody>
           {console.log(data.length)}
           { data.length ?
-          testData.map((data)=>
-            <tr key={data.id}>
-              <td><a href={'http://localhost:3000/tests/'+data.testName}>{data.testName}</a></td>
-              <td>{data.updateDate}</td>
-              <td>{data.users}</td>
-              <td><a href='#' onClick={handleClick}>▶ Run Test</a></td>
+          testData.map((data, index)=>
+            <tr key={index}>
+              <td className='list-item'><a href={'http://localhost:3000/tests/'+data.testName} onClick={handleTestClick}>{data.testName}</a></td>
+              <td className='list-item'>{data.updateDate}</td>
+              <td className='list-item'>{
+                /* Maps out the list of users... */
+                data.ThreadGroups.map((tg_data, index) => {
+                  return <div key={index} className='list-item'>
+                    {index ? "/"+tg_data.num_threads : tg_data.num_threads}
+                  </div>
+                })
+              }</td>
+              <td className='list-item'>{
+                /* Maps out the list of durations... */
+                data.ThreadGroups.map((tg_data, index) => {
+                  return <div key={index} className="list-item">
+                    {index ? "/"+returnDuration(tg_data.duration) : returnDuration(tg_data.duration)}
+                  </div>
+                })
+              }</td>
+              <td className='list-item'><a href='#' onClick={handleRunTestClick}>▶ Run Test</a></td>
+              <td className='list-item'><a href='#' onClick={handleReportClick}>Report</a></td>
             </tr>
           )
           :<div>empty</div>}
